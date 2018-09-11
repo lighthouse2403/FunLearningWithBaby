a5cd42d29d0dcea531b7f0a548f0d63c33a84203
Hai Dang <dangnh@wav.vn>
2018-09-11 17:24:00 +0700

    no message

--- FunLearningWithBaby/FunLearningWithBaby/ViewController/ManagerPictureLesson/ManagerPictureViewController.swift
+++ FunLearningWithBaby/FunLearningWithBaby/ViewController/ManagerPictureLesson/ManagerPictureViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NewLessonViewController: OriginalViewController {
+class ManagerPictureViewController: OriginalViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
