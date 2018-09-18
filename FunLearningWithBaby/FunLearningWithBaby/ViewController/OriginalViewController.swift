77c050565cbc0ae34a50caf10de705733e68cd11
Hai Dang <dangnh@wav.vn>
2018-09-18 16:02:33 +0700

    no message

--- FunLearningWithBaby/FunLearningWithBaby/ViewController/OriginalViewController.swift
+++ FunLearningWithBaby/FunLearningWithBaby/ViewController/OriginalViewController.swift
@@ -26,7 +26,6 @@ class OriginalViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     //MARK: - Function
